969d43a5c3a7f9c66f7748f1e3b0549efb280f53
ioan grozea <ioan.grozea@gmail.com>
2020-11-02 13:01:31 +0200

    -created entity

--- src/main/java/com/internshipfinder/demo/repository/StudentProfileRepository.java
+++ src/main/java/com/internshipfinder/demo/repository/StudentProfileRepository.java
@@ -1,7 +1,7 @@
 package com.internshipfinder.demo.repository;
 
-import com.internshipfinder.demo.entity.Requirement;
+import com.internshipfinder.demo.entity.StudentProfile;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentProfileRepository extends CrudRepository<Requirement, Long> {
+public interface StudentProfileRepository extends CrudRepository<StudentProfile, Long> {
 }
